b999eb1f14c1f11e63ec508d4a209567c199541f
Thiago Zilio <tzilioassuncao@gmail.com>
2025-01-13 18:28:31 -0300

    database quase pronto

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+require("dotenv").config();
+console.log(process.env.DB_HOST);
+
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 3001;
+
+require("dotenv").config();
+console.log('Host:', process.env.PG_HOST); // Deve exibir o valor definido no .env
+
 
 // Configuração para servir arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
--- src/application/app.js
+++ src/application/app.js
@@ -1,17 +1,19 @@
-require('dotenv').config();
+require("dotenv").config({path: '../../.env'});
 const express = require('express');
-const mysql = require('mysql2');
+const { Pool } = require('pg'); // Importa o cliente para PostgreSQL
 const router = express.Router();
 
 // Configuração do banco de dados
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+const db = new Pool({
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASS,
+    database: process.env.PG_DATABASE,
+    port: process.env.PG_PORT,
+    ssl: { rejectUnauthorized: false }
 });
 
-// Conectar ao banco de dados
+// Testar conexão ao banco de dados
 db.connect((err) => {
     if (err) {
         console.error('Erro ao conectar ao banco de dados:', err);
@@ -21,27 +23,25 @@ db.connect((err) => {
 });
 
 // Rota para listar produtos (com ou sem busca)
-router.get('/products', (req, res) => {
+router.get('/products', async (req, res) => {
     let searchQuery = req.query.q;
     let sql = 'SELECT * FROM bearings';
     let params = [];
 
     // Se houver uma busca, adicionar a cláusula WHERE
     if (searchQuery) {
-        sql += ' WHERE descricao LIKE ? OR marca LIKE ? OR codigo LIKE ?';
+        sql += ' WHERE descricao ILIKE $1 OR marca ILIKE $2 OR codigo ILIKE $3';
         searchQuery = `%${searchQuery}%`;
         params = [searchQuery, searchQuery, searchQuery];
     }
 
-    db.query(sql, params, (err, results) => {
-        if (err) {
-            console.error('Erro na consulta de produtos:', err);
-            res.status(500).send('Erro ao buscar produtos.');
-        } else {
-            res.render('products', { produtos: results, q: searchQuery });
-        }
-    });
+    try {
+        const results = await db.query(sql, params); // Executa a consulta
+        res.render('products', { produtos: results.rows, q: searchQuery }); // Renderiza com os resultados
+    } catch (err) {
+        console.error('Erro na consulta de produtos:', err);
+        res.status(500).send('Erro ao buscar produtos.');
+    }
 });
-
 
 module.exports = router;
